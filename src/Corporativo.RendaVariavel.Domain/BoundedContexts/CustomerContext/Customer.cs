ba1835af008ae491a8afe9906bbc78345899de7a
Otavio Carmanini <otaviovb.developer@gmail.com>
2025-07-03 07:48:34 -0300

    feature: create projets on solution

---
+++ src/Corporativo.RendaVariavel.Domain/BoundedContexts/CustomerContext/Customer.cs
@@ -0,0 +1,17 @@
+﻿namespace Corporativo.RendaVariavel.Domain.BoundedContexts.CustomerContext;
+
+public sealed record Customer
+{
+    public Customer(string firstName, string lastName, DateTime birthDate, string email)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        BirthDate = birthDate;
+        Email = email;
+    }
+
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime BirthDate { get; set; }
+    public string Email { get; set; }
+}
