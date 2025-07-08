82437303aa21d27ea0b8b70ac4c80793529b7dec
Otavio Carmanini <otaviovb.developer@gmail.com>
2025-07-08 20:53:08 -0300

    feature: add unit tests for retriable producing messages

---
+++ benchs/ProducerSerializator/Program.cs
@@ -0,0 +1,75 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Running;
+using ProtoBuf;
+using System.Text;
+using System.Text.Json;
+
+namespace ProducerSerializator;
+
+public sealed class Program
+{
+    static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+    }
+}
+
+[MemoryDiagnoser]
+public class SerializationBenchmark
+{
+    private List<Message> _messages;
+
+    [Params(100_000)]
+    public int RunCount { get; set; }
+
+    [Benchmark(Baseline = true, Description = "Json")]
+    public void JsonSerialization()
+    {
+        var message = new Message()
+        {
+            Title = "0197eac2-d14d-79a9-bed6-ee1c4361ce38",
+            Body = "0197eac2-d14d-79a9-bed6-ee1c4361ce38",
+            Author = "0197eac2-d14d-79a9-bed6-ee1c4361ce38"
+        };
+
+        for (int i = 0; i < RunCount; i++)
+        {
+            JsonSerializer.Serialize(message);
+        }
+    }
+
+    [Benchmark(Description = "Protobuf")]
+    public void ProtobufSerialization()
+    {
+        var message = new Message()
+        {
+            Title = "0197eac2-d14d-79a9-bed6-ee1c4361ce38",
+            Body = "0197eac2-d14d-79a9-bed6-ee1c4361ce38",
+            Author = "0197eac2-d14d-79a9-bed6-ee1c4361ce38"
+        };
+
+        for (int i = 0; i < RunCount; i++)
+        {
+            using var memoryStream = new MemoryStream();
+
+            Serializer.Serialize(memoryStream, message);
+
+            Encoding.UTF8.GetString(memoryStream.ToArray());
+        }
+    }
+}
+
+[ProtoContract()]
+public sealed class Message
+{
+    [ProtoMember(1)]
+    public string Title { get; set; }
+
+    [ProtoMember(2)]
+    public string Body { get; set; }
+
+    [ProtoMember(3)]
+    public string Author { get; set; }
+}
---
+++ tsts/Corporativo.RendaVariavel.UnitTests/Infrascructure/ApacheKafka/RetriableProducerTests.cs
@@ -0,0 +1,127 @@
+﻿using Confluent.Kafka;
+using Corporativo.RendaVariavel.Infrascructure.ApacheKafka.Producers;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Corporativo.RendaVariavel.UnitTests.Infrascructure.ApacheKafka;
+
+public sealed class RetriableProducerTests
+{
+    private readonly Mock<ILogger<RetriableProducer<RetriableProducerFakeObject>>> _mockLogger = new();
+    private readonly Mock<IProducer<string, string>> _mockKafkaProducer = new();
+
+    public sealed record RetriableProducerFakeObject
+    {
+        public string Message { get; set; } = string.Empty;
+    }
+
+    private RetriableProducer<RetriableProducerFakeObject> CreateProducer(ProducerConfiguration config)
+        => new RetriableProducer<RetriableProducerFakeObject>(
+            _mockLogger.Object,
+            _mockKafkaProducer.Object,
+            config);
+
+    [Fact]
+    public async Task GivenRequestRetriableProduceAsync_ShouldCallProducerAsExpected()
+    {
+        // Arrange
+        var config = new ProducerConfiguration { TopicName = "test-topic" };
+
+        var producer = CreateProducer(config);
+        var key = "my-key";
+        var message = new RetriableProducerFakeObject() { Message = "my-message" };
+
+        _mockKafkaProducer
+            .Setup(p => p.ProduceAsync(
+                It.IsAny<string>(),
+                It.IsAny<Message<string, string>>(),
+                It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new DeliveryResult<string, string>());
+
+        // Act
+        await producer.ProduceAsync(key, message, CancellationToken.None);
+
+        // Assert
+        _mockKafkaProducer.Verify(p =>
+            p.ProduceAsync(
+                It.Is<string>(p => p == "test-topic"),
+                It.Is<Message<string, string>>(m => m.Key == key && m.Value.Contains("my-message")),
+                It.IsAny<CancellationToken>()),
+            Times.Once);
+    }
+
+    [Fact]
+    public async Task GivenRequestRetriableProduceAsyncThrowsAnyError_ShouldCallRetryingProducerUntilMaxAttempts()
+    {
+        // Arrange
+        var config = new ProducerConfiguration
+        {
+            TopicName = "retry-topic",
+            Retriable = new RetriableProducerConfiguration
+            {
+                RetryCount = 3,
+                DelayInMiliseconds = 1
+            }
+        };
+
+        var producer = CreateProducer(config);
+        var key = "retry-key";
+        var message = new RetriableProducerFakeObject() { Message = "retry-message" };
+        var attempts = 0;
+
+        _mockKafkaProducer
+            .Setup(p => p.ProduceAsync(It.IsAny<string>(), It.IsAny<Message<string, string>>(), It.IsAny<CancellationToken>()))
+            .Callback(() => attempts++)
+            .ThrowsAsync(new Exception("Kafka is down"));
+
+        // Act
+        await producer.ProduceRetriableAsync(key, message);
+
+        // Assert
+        Assert.Equal(config.Retriable.RetryCount + 1, attempts);
+        _mockLogger.Verify(
+            x => x.Log(
+                LogLevel.Warning,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, _) => v.ToString()!.Contains("has been failed")),
+                It.IsAny<Exception>(),
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
+            Times.AtLeastOnce);
+    }
+
+    [Fact]
+    public async Task ProduceRetriableAsync_Should_Log_Error_After_All_Retries()
+    {
+        // Arrange
+        var config = new ProducerConfiguration
+        {
+            TopicName = "retry-topic",
+            Retriable = new RetriableProducerConfiguration
+            {
+                RetryCount = 2,
+                DelayInMiliseconds = 1
+            }
+        };
+
+        var producer = CreateProducer(config);
+        var key = "retry-key";
+        var message = new RetriableProducerFakeObject() { Message = "error-retry-message" };
+
+        _mockKafkaProducer
+            .Setup(p => p.ProduceAsync(It.IsAny<string>(), It.IsAny<Message<string, string>>(), It.IsAny<CancellationToken>()))
+            .ThrowsAsync(new Exception("Kafka error"));
+
+        // Act
+        await producer.ProduceRetriableAsync(key, message);
+
+        // Assert
+        _mockLogger.Verify(
+            x => x.Log(
+                LogLevel.Error,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, _) => v.ToString()!.Contains("All retries has been failed")),
+                It.IsAny<Exception>(),
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
+            Times.Once);
+    }
+}
